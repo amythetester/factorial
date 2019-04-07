8e246cfcd85650c167189b63e5d2cd964104433e
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-06 20:47:34 -0700

    factorial can be processed

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,14 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is how large the number is.
+# Space complexity: O(1)
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil
+  return 1 if number == 0 || number == 1
+
+  factorial = number
+  while number > 1
+    number -= 1
+    factorial *= number
+  end
+  return factorial
 end
